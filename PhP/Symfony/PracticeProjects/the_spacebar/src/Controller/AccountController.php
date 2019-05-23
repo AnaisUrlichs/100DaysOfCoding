debed9250dea7787fe2e39695da66af757250834
AnaisUrlichs <33576047+AnaisUrlichs@users.noreply.github.com>
2019-05-23 16:16:02 +0200

    Create AccountController.php

---
+++ PhP/Symfony/PracticeProjects/the_spacebar/src/Controller/AccountController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Controller;
+
+use Faker\Provider\Base;
+use Psr\Log\LoggerInterface;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Response;
+
+/**
+ * @IsGranted("ROLE_USER")
+ */
+
+class AccountController extends BaseController
+{
+    /**
+     * @Route("/account", name="app_account")
+     */
+
+    public function index(LoggerInterface $logger)
+    {
+        $logger->debug('Checking account page for '.$this->getUser()->getEmail());
+        return $this->render('account/index.html.twig', [
+        ]);
+    }
+
+    /**
+     * @Route("/api/account", name="api_account")
+     */
+    public function accountApi()
+    {
+        $user = $this->getUser();
+        return $this->json($user, 200, [], [
+            'groups' => ['main'],
+        ]);
+    }
+}
