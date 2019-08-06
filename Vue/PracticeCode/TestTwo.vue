0e3537cb3c310193d383c31fb4c3c5a2d073542b
AnaisUrlichs <33576047+AnaisUrlichs@users.noreply.github.com>
2019-08-06 10:00:10 +0200

    Create TestTwo.vue

---
+++ Vue/PracticeCode/TestTwo.vue
@@ -0,0 +1,42 @@
+<template>
+  <div id="root">
+    Sign-up
+    <br />
+    <p v-once>{{email}}</p>
+    <p v-text="email"></p>
+    <br />
+    <input v-model="email" :class="{red: email.length < 2}" />
+    <br />
+    <button onclick="alert('signed up')" :disabled="email.length < 2 ">Submit</button>
+  </div>
+</template>
+
+<script>
+export default {
+  el: "#root",
+  data: function() {
+    return {
+      email: ""
+    };
+  }
+};
+</script>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+}
+
+body {
+  font-family: Arial, Helvetica, sans-serif;
+}
+
+.red {
+  border: 2px solid red;
+}
+
+.green {
+  border: 2px solid greed;
+}
+</style>
