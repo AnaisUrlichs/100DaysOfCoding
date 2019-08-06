0463f8eca2d2c32453164aafcb6db7f3fb3d7ff1
AnaisUrlichs <33576047+AnaisUrlichs@users.noreply.github.com>
2019-08-06 09:42:43 +0200

    Create TestOne.vue

---
+++ Vue/PracticeCode/TestOne.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="app">
+    <div v-if="count === 0">Green</div>
+    <div v-else-if="count === 1">Green</div>
+    <div v-else>No data provided</div>
+
+    <div v-show="count === 3">This is a lucky number</div>
+
+    <div>
+      <p>{{count}}</p>
+      <input type="text" v-model="count" />
+      <input type="submit" />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  el: "#app",
+  data: function() {
+    return {
+      count: 3
+    };
+  }
+};
+</script>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+}
+
+body {
+  font-family: Arial, Helvetica, sans-serif;
+}
+</style>
